e9b7b97b7ab58383a49cbe951ae67dcfcbecf093
OmegaWolf <omega.wolf0401@gmail.com>
2025-07-06 17:29:25 +0200

    Premier commit. Création de la connexion à la base de donnée, de la classe registre ainsi que de la classe application avec la propriété update pour se synchro avec la base de donnée. Manque l'intégration des notes.

---
+++ main.py
@@ -0,0 +1,60 @@
+import sqlite3 as sql
+
+class Data:
+    def __init__(self,data_path):
+        self.conn = sql.connect(data_path)
+        
+    def request(self,command):
+        cur = self.conn.cursor()
+        response = cur.execute(command)
+        list_response = list(response)
+        cur.close()
+        self.conn.commit()
+        return list(list_response)
+    
+    def close(self):
+        self.conn.close()
+
+class Register:
+    def __init__(self, id, id_link, name):
+        self.id = id
+        self.id_link = id_link
+        self.name = name
+        
+
+class App:
+    def __init__(self, data:Data, user_list = None, year_list = None, trimester_list = None, matter_list = None, rate_list =None):
+        
+        self.data = data
+        
+        self.user_list = user_list if user_list != None else []
+        self.year_list = year_list if year_list != None else []
+        self.trimester_list = trimester_list if trimester_list != None else []
+        self.matter_list = matter_list if matter_list != None else []
+        self.rate_list = rate_list if rate_list != None else []
+        
+        self.all_lists = [self.user_list, self.year_list, self.trimester_list, self.matter_list, self.rate_list]
+        
+    def update(self):
+        [group.clear for group in self.all_lists]
+        for user_data in self.data.request("SELECT * FROM user"):
+            self.user_list.append(Register(user_data[0],None, user_data[1]))
+        for year_data in self.data.request("SELECT * FROM year"):
+            self.year_list.append(Register(year_data[0], year_data[1], year_data[2]))
+        for trimester_data in self.data.request("SELECT * FROM trimester"):
+            self.trimester_list.append(Register(trimester_data[0],trimester_data[1],trimester_data[2]))
+        for matter_data in self.data.request("SELECT * FROM matter"):
+            self.matter_list.append(Register(matter_data[0], matter_data[1], matter_data[2]))
+        for rate_data in self.data.request("SELECT * FROM rate"):
+            pass
+            
+        
+db = Data("data.db")
+
+app = App(db)
+app.update()
+    
+db.close()
+    
+        
+
